51f9a9ce132a2949951b460ba1271e98043bbe08
mhmdaril707-pixel <mhmdaril707@gmail.com>
2026-08-08 20:33:03 +0700

    Implement dark mode styling in theme.js

--- js/theme.js
+++ js/theme.js
@@ -14,7 +14,29 @@
         data.tema || "light";
 
     if (tema === "dark") {
+
         document.body.classList.add("dark-mode");
+
+        document.documentElement.style.backgroundColor =
+            "#121212";
+
+        document.body.style.setProperty(
+            "background-color",
+            "#121212",
+            "important"
+        );
+
+    } else {
+
+        document.body.classList.remove("dark-mode");
+
+        document.documentElement.style.backgroundColor =
+            "";
+
+        document.body.style.removeProperty(
+            "background-color"
+        );
+
     }
 
 })();
