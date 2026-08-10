2cd98a88a7509645dc14b249e948b67c7ba0a4bb
alfattahfinance <mhmdaril707@gmail.com>
2026-08-11 01:26:34 +0700

    Implement authentication guard in auth-guard.js

---
+++ js/auth-guard.js
@@ -0,0 +1,21 @@
+// ======================================
+// Syahriyyah / Alfattah Finance
+// AUTH GUARD
+// ======================================
+
+import { auth } from "./firebase-config.js";
+
+import {
+    onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.17.1/firebase-auth.js";
+
+
+onAuthStateChanged(auth, (user) => {
+
+    if (!user) {
+
+        window.location.replace("login.html");
+
+    }
+
+});
