0360ae383e8a8ea6ab9a88662dc78db3c7a13f51
alfattahfinance <mhmdaril707@gmail.com>
2026-08-11 01:56:31 +0700

    Remove unnecessary whitespace and fix formatting

--- js/auth-guard.js
+++ js/auth-guard.js
@@ -9,13 +9,10 @@ import {
     onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-auth.js";
 
-
 onAuthStateChanged(auth, (user) => {
 
     if (!user) {
-
         window.location.replace("login.html");
-
     }
 
 });
